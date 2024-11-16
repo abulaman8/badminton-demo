31b65eb62df41a54335ed2f015ddcd879827b78f
abulaman8 <abulaman6@gmail.com>
2024-11-16 17:45:08 +0530

    collected static

--- base/urls.py
+++ base/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.sigin, name='login'),
     path('logout/', views.signout, name='logout'),
